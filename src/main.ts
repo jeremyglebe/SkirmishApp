2a0205e7c7459e34cf0f6ce7538c311a8011b068
Jeremy D Glebe <jeremyglebe@gmail.com>
2023-04-01 23:05:25 -0500

    deployed as a pwa now

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage-angular';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -15,7 +16,16 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    importProvidersFrom(IonicModule.forRoot({}), IonicStorageModule.forRoot()),
+    importProvidersFrom(
+      IonicModule.forRoot({}),
+      IonicStorageModule.forRoot(),
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: environment.production,
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000',
+      })
+    ),
     provideRouter(routes),
   ],
 });
