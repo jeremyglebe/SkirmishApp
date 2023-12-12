b0a138eb0d9a1847e23d9365fa421618f94ca59e
Jeremy Glebe <jeremyglebe@gmail.com>
2023-12-12 13:08:51 -0600

    unit v2 modeled for new unit creation

--- src/app/data/models.ts
+++ src/app/data/models.ts
@@ -21,3 +21,28 @@ export interface Warband {
 
 export const DEFAULT_UNIT_IMAGE =
   'data:image/png;base64,your_base64_image_string_here';
+
+export enum UnitSize {
+  Small,
+  Standard,
+  Large,
+  Huge,
+}
+
+export enum UnitSpeed {
+  Slow,
+  Standard,
+  Fast,
+}
+
+export interface UnitV2 {
+  id: string;
+  name: string;
+  rank: number;
+  attack: number;
+  defense: number;
+  size: UnitSize;
+  speed: UnitSpeed;
+  edges: Edge[];
+  image?: string; // Optional property for unit image
+}
